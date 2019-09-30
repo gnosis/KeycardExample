c6f4b1582018a94369d5aadaf0719f3bdcf65c7b
Dmitry Bespalov <dmitry.bespalov@gnosis.pm>
2019-09-30 16:47:44 +0200

    example streamlined

--- KeycardExample/ViewController.swift
+++ KeycardExample/ViewController.swift
@@ -9,25 +9,23 @@
 import UIKit
 import CoreNFC
 import Keycard
-import CryptoSwift
-import os
-import CommonCrypto
 
 class TableViewController: UITableViewController {
 
     var actions: [Action] = []
-    var nfcController: NFCController!
+
+    var keycardController: KeycardController?
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         actions = [
-            Action(name: "Get Status", closure: getStatus),
-            Action(name: "Initialize", closure: initialize),
+            Action(name: "Select", closure: { [unowned self] in self.select() }),
+            Action(name: "Initialize", closure: { [unowned self] in self.initialize() }),
         ]
 
         DispatchQueue.main.async {
-            if !NFCController.isAvailable {
+            if !KeycardController.isAvailable {
                 let alertController = UIAlertController(
                     title: "Scanning Not Supported",
                     message: "This device doesn't support tag scanning. The functionality is disabled.",
@@ -57,61 +55,43 @@ class TableViewController: UITableViewController {
         action.closure()
     }
 
-    func getStatus() {
-        guard nfcController == nil else { return }
-        nfcController = NFCController(message: "Hold your iPhone near a Status Keycard.")
-        nfcController.start(execute: { [unowned self] tag in
+    func select() {
+        keycardController = KeycardController(onConnect: { [unowned self] channel in
             do {
-                let channel = CoreNFCCardChannel(tag: tag)
                 let cmdSet = KeycardCommandSet(cardChannel: channel)
                 let info = try ApplicationInfo(cmdSet.select().checkOK().data)
-                let status = """
-                initialized: \(info.initializedCard ? "YES" : "NO")
-                instanceUID: \(Data(info.instanceUID).toHexString())
-                appVersion:  \(info.appVersionString)
-                freeSlots: \(info.freePairingSlots)
-                hasMasterKey: \(info.hasMasterKey ? "YES" : "NO")
-                keyUID: \(info.keyUID)
-                capabilities: \(info.capabilities)
-                secureChannel? \(info.hasSecureChannelCapability) keyManagement? \(info.hasKeyManagementCapability) credentialsManagement? \(info.hasCredentialsManagementCapability) ndef? \(info.hasNDEFCapability)
-                pubKey: \(Data(info.secureChannelPubKey).toHexString())
-                """
-                print(status)
-                tag.session?.alertMessage = "Success"
-                tag.session?.invalidate()
-                self.present(string: status)
+                print(info)
+                self.keycardController?.stop(alertMessage: "Success")
             } catch {
-                tag.session?.invalidate(errorMessage: "Read error. Please try again.")
-                self.present(string: "Error: \(error)")
+                print("Error: \(error)")
+                self.keycardController?.stop(errorMessage: "Read error. Please try again.")
             }
-            self.nfcController = nil
-        }, failure: { _ in
-            self.nfcController = nil
+            self.keycardController = nil
+        }, onFailure: { [unowned self] error in
+            print("Disconnected: \(error)")
+            self.keycardController = nil
         })
+        keycardController?.start(alertMessage: "Hold your iPhone near a Status Keycard.")
     }
 
     func initialize() {
-        guard nfcController == nil else { return }
-
         let initializeAction: (String, String, String) -> Void = { [unowned self] pin, puk, pass in
-            self.nfcController = NFCController(message: "Hold your iPhone near a Status Keycard.")
-            self.nfcController.start(execute: { [unowned self] tag in
-                tag.session?.alertMessage = "Initializatng... Please do not remove the Keycard."
+            self.keycardController = KeycardController(onConnect: { [unowned self] channel in
                 do {
-                    let channel = CoreNFCCardChannel(tag: tag)
                     let cmdSet = KeycardCommandSet(cardChannel: channel)
                     try cmdSet.select().checkOK()
                     try cmdSet.initialize(pin: pin, puk: puk, pairingPassword: pass).checkOK()
-                    tag.session?.alertMessage = "Success"
-                    tag.session?.invalidate()
+                    self.keycardController?.stop(alertMessage: "Success")
                 } catch {
-                    tag.session?.invalidate(errorMessage: "Read error. Please try again.")
+                    self.keycardController?.stop(errorMessage: "Read error. Please try again.")
                     self.present(string: "Error: \(error)")
                 }
-                self.nfcController = nil
-            }, failure: { _ in
-                self.nfcController = nil
+                self.keycardController = nil
+            }, onFailure: { [unowned self] error in
+                print("Disconnected: \(error)")
+                self.keycardController = nil
             })
+            self.keycardController?.start(alertMessage: "Hold your iPhone near a Status Keycard.")
         }
 
         let inputAlert = UIAlertController(
@@ -166,79 +146,4 @@ class TableViewController: UITableViewController {
 struct Action {
     var name: String
     var closure: () -> Void
-}
-
-class NFCController: NSObject, NFCTagReaderSessionDelegate {
-
-    static var isAvailable: Bool {
-        return NFCNDEFReaderSession.readingAvailable
-    }
-
-    let message: String
-    var session: NFCTagReaderSession!
-    var execute: ((NFCISO7816Tag) -> Void)!
-    var failure: ((Error) -> Void)!
-
-    init(message: String) {
-        self.message = message
-    }
-
-    func start(execute: @escaping (NFCISO7816Tag) -> Void, failure: @escaping (Error) -> Void) {
-        guard NFCController.isAvailable else { return }
-        self.execute = execute
-        self.failure = failure
-        session = NFCTagReaderSession(pollingOption: .iso14443, delegate: self)
-        session.alertMessage = message
-        session.begin()
-    }
-
-    // MARK: - NFCTagReaderSessionDelegate
-
-    func tagReaderSessionDidBecomeActive(_ session: NFCTagReaderSession) {
-        // no-op
-    }
-
-    func tagReaderSession(_ session: NFCTagReaderSession, didInvalidateWithError error: Error) {
-        failure?(error)
-    }
-
-    func tagReaderSession(_ session: NFCTagReaderSession, didDetect tags: [NFCTag]) {
-        if tags.count > 1 {
-            session.alertMessage = "More than one tag was found. Please present only one tag."
-            tagRemovalDetect(tags[0])
-            return
-        }
-        guard let first = tags.first, case NFCTag.iso7816(let tag) = first else {
-            session.invalidate(errorMessage: "Unsupported Smart Card.")
-            return
-        }
-        session.connect(to: first) { [weak self] error in
-            if let error = error {
-                print("Connection error: \(error)")
-                session.invalidate(errorMessage: "Connection error. Please try again.")
-                return
-            }
-            DispatchQueue.global().async {
-                self?.execute?(tag)
-            }
-        }
-    }
-
-    func tagRemovalDetect(_ tag: NFCTag) {
-        // In the tag removal procedure, you connect to the tag and query for
-        // its availability. You restart RF polling when the tag becomes
-        // unavailable; otherwise, wait for certain period of time and repeat
-        // availability checking.
-        self.session?.connect(to: tag) { [weak self] error in
-            guard let `self` = self else { return }
-            guard error == nil && tag.isAvailable else {
-                self.session?.restartPolling()
-                return
-            }
-            DispatchQueue.global().asyncAfter(deadline: DispatchTime.now() + .milliseconds(500), execute: {
-                self.tagRemovalDetect(tag)
-            })
-        }
-    }
-
 }
